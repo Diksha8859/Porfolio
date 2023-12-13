b4f83edf0d57b682026296bb93b61fe0f13b8061
Diksha Shashwat <diksha2021128@gmail.com>
2023-12-13 20:33:14 +0530

    first commit

---
+++ my-portfolio/src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,38 @@
+import './portfolio.scss'
+
+export default function Portfolio() {
+  return (
+    <div className='portfolio' id='portfolio'>
+      <h1>Portfolio</h1>
+      <ul>
+        <li className='active'>Featured</li>
+        <li>Web App</li>
+        <li>Design</li>
+        <li>Mobile App</li>
+        <li>Branding</li>
+      </ul>
+      <div className='container'>
+        <div className='item'>
+          <img src="https://imgs.search.brave.com/wXbGr_31aMJt4iXpfK9JfEF553yJ30OH7O_gagKfGqw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxNi8w/OC8xNS8xNy8zOC9w/aWdneS1iYW5rLTE1/OTU5OTJfNjQwLmpw/Zw" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className='item'>
+          <img src="https://imgs.search.brave.com/wXbGr_31aMJt4iXpfK9JfEF553yJ30OH7O_gagKfGqw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxNi8w/OC8xNS8xNy8zOC9w/aWdneS1iYW5rLTE1/OTU5OTJfNjQwLmpw/Zw" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className='item'>
+          <img src="https://imgs.search.brave.com/wXbGr_31aMJt4iXpfK9JfEF553yJ30OH7O_gagKfGqw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxNi8w/OC8xNS8xNy8zOC9w/aWdneS1iYW5rLTE1/OTU5OTJfNjQwLmpw/Zw" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className='item'>
+          <img src="https://imgs.search.brave.com/wXbGr_31aMJt4iXpfK9JfEF553yJ30OH7O_gagKfGqw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxNi8w/OC8xNS8xNy8zOC9w/aWdneS1iYW5rLTE1/OTU5OTJfNjQwLmpw/Zw" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className='item'>
+          <img src="https://imgs.search.brave.com/wXbGr_31aMJt4iXpfK9JfEF553yJ30OH7O_gagKfGqw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxNi8w/OC8xNS8xNy8zOC9w/aWdneS1iYW5rLTE1/OTU5OTJfNjQwLmpw/Zw" alt="" />
+          <h3>Banking App</h3>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ my-portfolio/src/components/topbar/Topbar.jsx
@@ -0,0 +1,30 @@
+import './topbar.scss'
+import { Person, Mail } from '@mui/icons-material';
+
+export default function Topbar({menuOpen, setMenuOpen}) {
+  return (
+    <div className={'topbar ' + (menuOpen && "active")}>
+       <div className='wrapper'>
+         <div className="left">
+           <a href='#intro' className='logo' >genius.</a>
+           <div className='itemContainer'> 
+            <Person /> 
+            <span>+44 876 89</span>
+           </div>
+           <div className='itemContainer'>
+            <Mail /> 
+            <span>diksha@gmail.com</span>
+           </div>
+         </div>
+        
+         <div className="right">
+           <div className='hamburger' onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className='line1'></span>
+            <span className='line2'></span>
+            <span className='line3'></span>
+           </div>
+         </div>
+       </div>
+        </div>
+  )
+}
